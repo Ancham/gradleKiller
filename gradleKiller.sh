658ed1d288154e885e0a4aa9aba3de77c1241262
MP <mateuszp181@gmail.com>
2021-04-12 16:51:58 +0200

    Add files via upload

--- gradleKiller.sh
+++ gradleKiller.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 
+$(notify-send "gradleKiller started")
 regexp='^[0-9]+$'
+killedProcesses=()
 if ! [[ $1 =~ $regexp ]] ; then
+    echo "Put number parameter"
     exit 1
 else
     while true
     do
-        pids_array=($(pidof java))
-        if [ ${#pids_array[@]} -gt 1 ] ; then
-            unset pids_array[-1]
-            pids_array_size=${#pids_array[@]}
-            for i in ${pids_array[@]}
+        pidsArray=($(pidof java))
+        if [ ${#pidsArray[@]} -gt 1 ] ; then
+            unset pidsArray[-1]
+            pids_array_size=${#pidsArray[@]}
+            for i in ${pidsArray[@]}
             do
                 pid_state=($(ps -q $i -o state))
                 if [ $pid_state == 'S' ] ; then
+                    echo "$(date +'%H:%M:%S') - trying to kill $i"
+                    killedProcesses+="$i | " 
                     kill ${i}
+                else
+                    echo "There is nothing to kill"
                 fi
+            $(notify-send "Killed processes - ${killedProcesses[@]}")
+            unset killedProcesses
             done
         fi
         sleep $1
     done
 fi
+
+
+
 
 
 
